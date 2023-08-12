a011869c09f481d41990bd21edae8ad50c1544af
meetsandesh <sandesh3008@gmail.com>
2023-08-12 16:34:19 +0530

    Login Page, Sign up Page, navigation

--- Assignment_Solution_Frontend_Code/ecommerce-upgrad-frontend/src/App.js
+++ Assignment_Solution_Frontend_Code/ecommerce-upgrad-frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import PageSetUp from "./components/pageSetUp/PageSetUp";
+import {useDispatch} from "react-redux";
+import {useCallback, useEffect} from "react";
+import {initCatalog} from "./store/actions/metadataAction";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#3f51b5',
+		},
+		secondary: {
+			main: '#f50057',
+		},
+		disabled: {
+			main: "#56595c",
+		}
+	},
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const dispatch = useDispatch();
+	const initPageData = useCallback((dispatchEvent = dispatch) => {
+		dispatchEvent(initCatalog());
+	}, [dispatch]);
+	useEffect(() => {
+		initPageData();
+	}, [initPageData]);
+	return (
+		<ThemeProvider theme={theme}>
+			<PageSetUp />
+		</ThemeProvider>
+	);
 }
 
 export default App;
