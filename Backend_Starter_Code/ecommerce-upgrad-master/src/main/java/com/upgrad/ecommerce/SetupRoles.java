7439f92e7c35bac657ced8482a37244da5a37725
meetsandesh <sandesh3008@gmail.com>
2023-08-12 15:18:19 +0530

    Updated Admin User creation

--- Backend_Starter_Code/ecommerce-upgrad-master/src/main/java/com/upgrad/ecommerce/SetupRoles.java
+++ Backend_Starter_Code/ecommerce-upgrad-master/src/main/java/com/upgrad/ecommerce/SetupRoles.java
@@ -46,20 +46,24 @@ public class SetupRoles implements ApplicationListener<ApplicationReadyEvent> {
         }
 
         //create admin user, after creating all roles
-        try {
-            User user = new User();
-            user.setFirstName("Super");
-            user.setLastName("Admin");
-            user.setEmail("admin@gmail.com");
-            user.setPassword(encoder.encode("Qwerty@123"));
-            Set<Role> roles = new HashSet<>();
-            Role userRole = roleRepository.findByName(ERole.ADMIN).orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(userRole);
-            user.setRoles(roles);
-            user.setContactNumber("0123456789");
-            userRepository.save(user);
-            System.out.println("Admin User Created " + user.getEmail());
-        } catch (Exception e) {
+        if(userRepository.findByEmail("admin@gmail.com") == null) {
+            try {
+                User user = new User();
+                user.setFirstName("Super");
+                user.setLastName("Admin");
+                user.setEmail("admin@gmail.com");
+                user.setPassword(encoder.encode("Qwerty@123"));
+                Set<Role> roles = new HashSet<>();
+                Role userRole = roleRepository.findByName(ERole.ADMIN).orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                roles.add(userRole);
+                user.setRoles(roles);
+                user.setContactNumber("0123456789");
+                userRepository.save(user);
+                System.out.println("Admin User Created " + user.getEmail());
+            } catch (Exception e) {
+                System.out.println("Could not create admin user");
+            }
+        } else {
             System.out.println("Skipping Admin User Creation");
         }
     }
